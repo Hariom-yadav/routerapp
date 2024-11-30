066d3974d41d414df4bf3c97a7b2ab3b1b4a6022
Hariom-yadav <hariomyji@gmail.com>
2024-11-30 18:24:27 +0530

    deploy commit router3

--- src/App.js
+++ src/App.js
@@ -9,82 +9,82 @@ import MainHeader from './components/MainHeader';
 import NotFound from './components/NotFound';
 import { NavLink} from 'react-router-dom';
 
-const router = createBrowserRouter(
-  [
-    { path: "/", element:<Home />  }, // Redirect to "/login"
-    { path: "/About", element: <About /> },
-    { path: "/Home", element: <Home /> },
-    { path: "/Labs", element: <Labs /> },
-    { path: "/Supports", element: <Supports /> },
-    { path: "*", element: <NotFound /> }, // Fallback for unknown routes
-  ],
-  {
-    future: {
-      v7_relativeSplatPath: true, // Enables relative paths in nested routes
-      v7_fetcherPersist: true,   // Retains fetcher state during navigation
-      v7_normalizeFormMethod: true, // Normalizes form methods (e.g., POST or GET)
-      v7_partialHydration: true, // Supports partial hydration for server-side rendering
-      v7_skipActionErrorRevalidation: true, // Prevents revalidation when action errors occur
-    },
-  }
-);
-
-function App() {
-  return (
-    <RouterProvider
-      future={{ v7_startTransition: true }} // Enables React's startTransition API
-      router={router}
-    />
-  );
+// const router = createBrowserRouter(
+//   [
+//     { path: "/", element:<Home />  }, // Redirect to "/login"
+//     { path: "/About", element: <About /> },
+//     { path: "/Home", element: <Home /> },
+//     { path: "/Labs", element: <Labs /> },
+//     { path: "/Supports", element: <Supports /> },
+//     { path: "*", element: <NotFound /> }, // Fallback for unknown routes
+//   ],
+//   {
+//     future: {
+//       v7_relativeSplatPath: true, // Enables relative paths in nested routes
+//       v7_fetcherPersist: true,   // Retains fetcher state during navigation
+//       v7_normalizeFormMethod: true, // Normalizes form methods (e.g., POST or GET)
+//       v7_partialHydration: true, // Supports partial hydration for server-side rendering
+//       v7_skipActionErrorRevalidation: true, // Prevents revalidation when action errors occur
+//     },
+//   }
+// );
 
 // function App() {
 //   return (
-//     <div className="App">
+//     <RouterProvider
+//       future={{ v7_startTransition: true }} // Enables React's startTransition API
+//       router={router}
+//     />
+//   );
 
-//     <div>
+function App() {
+  return (
+    <div className="App">
 
-//     <nav>
-//              <ul>
-//                   <li>
-//                   <NavLink to="/" >Home</NavLink>
-//                   </li>
+    <div>
+
+    <nav>
+             <ul>
+                  <li>
+                  <NavLink to="/" >Home</NavLink>
+                  </li>
                   
-//                   <li>
-//                   <NavLink to="/support" >Support</NavLink>
-//                   </li>
+                  <li>
+                  <NavLink to="/support" >Support</NavLink>
+                  </li>
 
-//                   <li>
-//                   <NavLink to="/about" >About</NavLink>
-//                   </li>
+                  <li>
+                  <NavLink to="/about" >About</NavLink>
+                  </li>
                   
-//                   <li>
-//                   <NavLink to="/lab" >Labs</NavLink>
-//                   </li>
-//             </ul>
-//     </nav>
+                  <li>
+                  <NavLink to="/lab" >Labs</NavLink>
+                  </li>
+            </ul>
+    </nav>
            
                  
 
             
 
-//             <Routes>
-//                       <Route path="/" element={<MainHeader/>} >
-//                             <Route index element={<Home/>}/>
-//                             <Route path="/support" element={<Supports/>} />
-//                             <Route path="/about" element={<About/>} />
-//                             <Route path="/lab" element={<Labs/>} />
-//                             <Route path="*" element={<NotFound/>} />
-//                       </Route>
+            <Routes>
+                      <Route path="/" element={<MainHeader/>} >
+                            <Route index element={<Home/>}/>
+                            <Route path="/support" element={<Supports/>} />
+                            <Route path="/about" element={<About/>} />
+                            <Route path="/lab" element={<Labs/>} />
+                            <Route path="*" element={<NotFound/>} />
+                      </Route>
           
           
-//           </Routes>
-//     </div>
+          </Routes>
+    </div>
 
 
    
 
-//     </div>
-//   );
+    </div>
+  );
 }
 
 export default App;
